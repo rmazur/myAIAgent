4de8d5663b33b93020537a9921a6410d0782e21c
Regis Mazur <regis_mazur@hotmail.com>
2025-05-17 23:36:40 +0100

    Initial commit

---
+++ myfirstagent.py
@@ -0,0 +1,13 @@
+# Super Simple Agent
+import ollama
+
+try:
+    response = ollama.generate(
+        model="deepseek-r1:latest",
+        prompt="What is an AI Transformer?"
+    )
+    print(response["response"])
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    # Optional: Check the ollama.log file for server-side issues
